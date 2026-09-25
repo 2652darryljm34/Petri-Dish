08a09269232578a64362301606de73dff3cbcbbb
2652darryljm34 <2652.darryljm34@gmail.com>
2026-09-25 07:41:25 -0400

    Delete petri-automaton directory

--- petri-automaton/stages/naive-snake.py
+++
@@ -1,7 +0,0 @@
-# the strategy the old stage rewarded: plant mycelium everywhere, snake forever
-while True:
-    harvest()
-    seed(MYCELIUM)
-    move(EAST)
-    if pos_x() == 0:
-        move(SOUTH)
--- petri-automaton/stages/s_culture.py
+++
@@ -1,6 +0,0 @@
-while True:
-    harvest()
-    seed(CULTURE)
-    move(EAST)
-    if pos_x() == 0:
-        move(SOUTH)
